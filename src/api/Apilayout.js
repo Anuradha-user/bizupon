eccc70721756cc8c72d6f4a8ed5309a78e98a8e4
Deepanshu Aggarwal <bizupondeepanshu@gmail.com>
2026-09-04 11:05:12 +0530

    chnages

--- src/api/Apilayout.js
+++ src/api/Apilayout.js
@@ -1,9 +1,9 @@
-// export const baseURL = "http://192.168.10.199:8002";
-export const baseURL = "http://192.168.10.199";
-
 // let USER_AUTH = `${baseURL}/api`;
 let USER_AUTH = `${baseURL}:8002/api`;
 let USER_REG = `${baseURL}:8010/api`;
+let MasterApi = `${baseURL}:8011/api`;
+let ProductList = `${baseURL}:8009/api;`
+
 
 
 const ApiLayout = {
@@ -17,6 +17,8 @@ const ApiLayout = {
   logout: `${USER_AUTH}/Account/logout`,
   refresh: `${USER_AUTH}/Account/refresh`,
   signup: `${USER_AUTH}/Account/signup`,
+  CarData: `${MasterApi}/Home/GetDDLMaster`,
+
 }
 
 export default ApiLayout;
