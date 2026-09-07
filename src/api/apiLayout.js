fe727b2290a06544023ac9070608185641800df0
Deepanshu Aggarwal <bizupondeepanshu@gmail.com>
2026-09-07 14:05:51 +0530

    changes

---
+++ src/api/apiLayout.js
@@ -0,0 +1,20 @@
+export const baseURL = "https://192.168.10.199";
+
+let USER_AUTH = `${baseURL}:8012/api`;
+let USER_REG = `${baseURL}:8110/api`;
+let MasterApi = `${baseURL}:8111/api`;
+
+const apiLayout = {
+// Auth Api
+login: `${USER_AUTH}/Account/Account`,
+register: `${USER_REG}/User/Registration`,
+emailVerification: `${USER_REG}/User/email-otp-verification`,
+emailResendOtp: `${USER_REG}/User/email-otp-resend`,
+
+logout: `${USER_AUTH}/Account/logout`,
+refresh: `${USER_AUTH}/Account/refresh`,
+signup: `${USER_AUTH}/Account/signup`,
+CarData: `${MasterApi}/Home/GetDDLMaster`,
+};
+
+export default apiLayout;
