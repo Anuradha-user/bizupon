d95556b4824a0b2cc59e3bab8983ec542720d158
Vikas Maurya <vikas.bizupone@gmail.com>
2026-09-07 13:02:30 +0530

    changed apiLayout.js naming

--- src/api/Apilayout.js
+++ src/api/Apilayout.js
@@ -1,20 +1,22 @@
-export const baseURL = "https://192.168.10.199";
+// export const baseURL = "http://192.168.10.199:8002";
+export const baseURL = "http://192.168.10.199";
 
-let USER_AUTH = `${baseURL}:8012/api`;
-let USER_REG = `${baseURL}:8110/api`;
-let MasterApi = `${baseURL}:8111/api`;
+// let USER_AUTH = `${baseURL}/api`;
+let USER_AUTH = `${baseURL}:8002/api`;
+let USER_REG = `${baseURL}:8010/api`;
 
-const ApiLayout = {
+
+const apiLayout = {
   // ****************** Auth Api ******************
   login: `${USER_AUTH}/Account/Account`,
   register: `${USER_REG}/User/Registration`,
   emailVerification: `${USER_REG}/User/email-otp-verification`,
-  emailResendOtp: `${USER_REG}/User/email-otp-resend`,
 
+  emailResendOtp: `${USER_REG}/User/email-otp-resend`,
+ 
   logout: `${USER_AUTH}/Account/logout`,
   refresh: `${USER_AUTH}/Account/refresh`,
   signup: `${USER_AUTH}/Account/signup`,
-  CarData: `${MasterApi}/Home/GetDDLMaster`,
-};
+}
 
-export default ApiLayout;
+export default apiLayout;
